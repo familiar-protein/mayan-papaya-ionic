0344da728a7298ad42a2f506ddd32fe6fa7a67f2
jan <jlhellmich@gmail.com>
2015-08-19 10:41:24 -0700

    Added profile view

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'Trivia'])
+angular.module('starter', ['ionic', 'starter.controllers', 'Trivia', 'Profile'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -38,6 +38,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'Trivia'])
       'menuContent': {
         templateUrl: 'templates/trivia.html',
         controller: 'TriviaController'
+      }
+    }
+  })
+
+  .state('app.profile', {
+    url: '/profile',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/profile.html',
+        controller: 'ProfileController'
       }
     }
   })
